34dea6a37073ee7c4ddb8da8eeeafffd3a3a1ccd
Naitik pitroda <pitroda.np@gmail.com>
2022-04-18 16:46:31 +0530

    changes

--- src/components/Firstsection.js
+++ src/components/Firstsection.js
@@ -1,32 +1,41 @@
-
-import '../Assets/css/Firsts.css';
-import mainimg from '../Assets/images/Image.png'
-function Firstsection(){
-    return (
-      <div id="Features" className="div_firstsec">
-        <div className="first_part">
-          <span> <b> <mark> Transforming </mark> the <br /> way higher
-            education <br /> tranditionally works </b> </span> <br /> <br />
-          Ever trought of how it would be if all the higer education is <br />
-          transformed? Now just stop wondering and start experiencing  <br /> <br />
-          <form>
-            <div class="row">
-              <div class="col col-9">
-                <input type="text" class="form-control" placeholder="First name" />
-              </div>
-              <div class="col col-2 me-3">
-                <button type="button" className="btn btn-primary col-12">
-                  Submit
-                </button>
-              </div>
+import "../Assets/css/Firsts.css";
+import mainimg from "../Assets/images/Image.png";
+function Firstsection() {
+  return (
+    <div id="Features" className="div_firstsec">
+      <div className="first_part">
+        <span>
+          {" "}
+          <b>
+            {" "}
+            <mark> Transforming </mark> the <br /> way higher education <br />{" "}
+            tranditionally works{" "}
+          </b>{" "}
+        </span>{" "}
+        <br /> <br />
+        Ever trought of how it would be if all the higer education is <br />
+        transformed? Now just stop wondering and start experiencing <br />{" "}
+        <br />
+        <form>
+          <div class="row">
+            <div class="col col-8">
+              <input type="text" class="form-control" placeholder="Email..." />
             </div>
-          </form>
-          Register yourself for beta test updates
-        </div>
-
-        <div className="second_part"><img src={mainimg} className='firstsec_img' /></div>
+            <div class="col col-2 me-3">
+              <button type="button" className="btn btn-primary col-12">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+        Register yourself for beta test updates
       </div>
-    )
+
+      <div className="second_part">
+        <img src={mainimg} className="firstsec_img" />
+      </div>
+    </div>
+  );
 }
 
-export default Firstsection
+export default Firstsection;
